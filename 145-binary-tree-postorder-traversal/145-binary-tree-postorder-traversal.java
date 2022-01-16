a5498265c46fc94783c88f19cc83b58ec670b31a
codernoob1 <79470721+codernoob1@users.noreply.github.com>
2022-01-16 20:30:17 +0530

    Update 145-binary-tree-postorder-traversal.java

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+/* This is using Stack */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
@@ -37,7 +38,7 @@ class Solution {
     }
 
 }
-/*
+/* This is using recursion */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
@@ -53,4 +54,3 @@ class Solution {
         ans.add(root.val);
     }
 }
-*/
