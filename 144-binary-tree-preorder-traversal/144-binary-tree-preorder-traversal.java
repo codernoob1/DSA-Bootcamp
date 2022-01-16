c19307169ebbb506e70a74f77552ce38fad54f87
codernoob1 <79470721+codernoob1@users.noreply.github.com>
2022-01-16 20:28:39 +0530

    Update 144-binary-tree-preorder-traversal.java

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+/*This is using stack*/
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
        List<Integer> ans = new ArrayList<>();
@@ -38,7 +39,7 @@ class Solution {
 
 }
 
-/*
+/*This is using recursion*/
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
        List<Integer> ans = new ArrayList<>();
