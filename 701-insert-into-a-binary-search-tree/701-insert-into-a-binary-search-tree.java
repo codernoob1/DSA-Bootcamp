adf8989b2a328add217823d325bee2195970738c
codernoob1 <79470721+codernoob1@users.noreply.github.com>
2022-01-27 00:32:21 +0530

    Time: 0 ms (100.00%), Space: 51.5 MB (32.29%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -16,6 +16,22 @@
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root == null) return new TreeNode(val);
+        
+        
+        if(root.val < val){ 
+            root.right = insertIntoBST(root.right,val);
+        }
+        else {
+            root.left =  insertIntoBST(root.left,val);
+        }
+        return root;
+    }
+}
+
+/*
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
         TreeNode curr = root;
         
         while(true){
@@ -36,3 +52,4 @@ class Solution {
         return root;
     }
 }
+*/
